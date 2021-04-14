385baaa866379ea8c311a06b23ee2f995f06e007
Rubens Junior <rubens.junior@outlook.com.br>
2021-04-14 19:27:35 -0300

    associação dos objetos, muitos para muitos

--- src/main/java/com/rubensjunior/cursomc/domain/Categoria.java
+++ src/main/java/com/rubensjunior/cursomc/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.rubensjunior.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable {
@@ -16,6 +19,10 @@ public class Categoria implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@ManyToMany(mappedBy = "categorias")
+	
+	private List<Produto> produtos = new ArrayList<>();
 
 	public Categoria() {
 	}
@@ -40,6 +47,14 @@ public class Categoria implements Serializable {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
 
 	@Override
@@ -66,6 +81,8 @@ public class Categoria implements Serializable {
 			return false;
 		return true;
 	}
+
+	
 
 	
 }
