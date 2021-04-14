dfd28060c8df9467c928a645a2742f2e2b743279
Rubens Junior <rubens.junior@outlook.com.br>
2021-04-14 20:55:03 -0300

    Ajuste end point /categorias /{id}

--- src/main/java/com/rubensjunior/cursomc/services/CategoriaService.java
+++ src/main/java/com/rubensjunior/cursomc/services/CategoriaService.java
@@ -2,11 +2,13 @@ package com.rubensjunior.cursomc.services;
 
 import java.util.Optional;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rubensjunior.cursomc.domain.Categoria;
 import com.rubensjunior.cursomc.repositories.CategoriaRepository;
+import com.rubensjunior.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -15,7 +17,14 @@ public class CategoriaService {
 	private CategoriaRepository repo;
 
 	public Categoria find(Integer id) {
-		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-	}
+		 Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		} 
+	
+	
+ 
+
 }
+
+
